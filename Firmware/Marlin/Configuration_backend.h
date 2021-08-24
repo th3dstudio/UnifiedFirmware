b5ca6c38e1c1414c463d10565c9bfa07ca630e14
Tim Hoogland <tim@th3dstudio.com>
2021-08-23 21:09:22 -0500

    Rename Board Config Folder + EZBoardV2 Prep
    Renamed board config folder to have no spaces.
    Change build directory settings for EZBoard V1 to use latest PIO standards.
    Prep for EZBoardV2

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.34a"
+#define UNIFIED_VERSION "TH3D UFW 2.35"
 
 /**
  * ABL Probe Settings

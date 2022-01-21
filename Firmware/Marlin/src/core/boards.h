a63cf51f4ebc5084d7dc2a639f82bfc502421e31
Tim Hoogland <timothy@hoogland.email>
2022-01-21 13:50:46 -0600

    Update EZABL Starting Gcode to V3

--- Firmware/Marlin/src/core/boards.h
+++ Firmware/Marlin/src/core/boards.h
@@ -333,12 +333,12 @@
 #define BOARD_CCROBOT_MEEB_3DP        4035  // ccrobot-online.com MEEB_3DP (STM32F103RC)
 #define BOARD_CHITU3D_V5              4036  // Chitu3D TronXY X5SA V5 Board
 #define BOARD_CHITU3D_V6              4037  // Chitu3D TronXY X5SA V6 Board
-#define BOARD_CREALITY_V4             4038  // Creality v4.x (STM32F103RE)
-#define BOARD_CREALITY_V427           4039  // Creality v4.2.7 (STM32F103RE)
-#define BOARD_CREALITY_V4210          4040  // Creality v4.2.10 (STM32F103RE) as found in the CR-30
-#define BOARD_CREALITY_V431           4041  // Creality v4.3.1 (STM32F103RE)
-#define BOARD_CREALITY_V452           4042  // Creality v4.5.2 (STM32F103RE)
-#define BOARD_CREALITY_V453           4043  // Creality v4.5.3 (STM32F103RE)
+#define BOARD_CREALITY_V4             4038  // Creality v4.x (STM32F103RC/STM32F103RE)
+#define BOARD_CREALITY_V427           4039  // Creality v4.2.7 (STM32F103RC/STM32F103RE)
+#define BOARD_CREALITY_V4210          4040  // Creality v4.2.10 (STM32F103RC/STM32F103RE) as found in the CR-30
+#define BOARD_CREALITY_V431           4041  // Creality v4.3.1 (STM32F103RC/STM32F103RE)
+#define BOARD_CREALITY_V452           4042  // Creality v4.5.2 (STM32F103RC/STM32F103RE)
+#define BOARD_CREALITY_V453           4043  // Creality v4.5.3 (STM32F103RC/STM32F103RE)
 #define BOARD_TRIGORILLA_PRO          4044  // Trigorilla Pro (STM32F103ZET6)
 #define BOARD_FLY_MINI                4045  // FLY MINI (STM32F103RCT6)
 #define BOARD_FLSUN_HISPEED           4046  // FLSUN HiSpeedV1 (STM32F103VET6)

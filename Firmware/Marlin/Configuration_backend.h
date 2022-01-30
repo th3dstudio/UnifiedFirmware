82e137fd9c563bebe6c4fbc91223317bbdaf54cb
Tim Hoogland <timothy@hoogland.email>
2022-01-29 22:31:43 -0600

    Bump version for next release
    Bump version for next release

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.37"
+#define UNIFIED_VERSION "TH3D UFW 2.38"
 
 /**
  * ABL Probe Settings

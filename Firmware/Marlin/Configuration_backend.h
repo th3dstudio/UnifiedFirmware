aa400846707c21932cf114d3ae626549c0ba25c6
Tim Hoogland <timothy@hoogland.email>
2022-01-31 14:15:14 -0600

    Update Configuration_backend.h

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.38"
+#define UNIFIED_VERSION "TH3D UFW 2.38T1"
 
 /**
  * ABL Probe Settings

a181ddaf2ceb8cd6785ed9287cd7f69dada7a7a0
Tim Hoogland <timothy@hoogland.email>
2022-03-05 19:22:25 -0600

    Revert "Creality V4 Boards with GD32 F303 Testing"
    This reverts commit ca03181c946f113a3fd378abe9f931f5707b7edc.

--- Internal Tools/Assemble Test Folders/CrealityV4X_512K_GD32.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rmdir "D:\Work\Printer Firmware\UF2TestBuilds\CrealityV4X_512K_GD32" /s /q
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\CrealityV4X_512K_GD32"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Board_Configs\CrealityV4X_512K_GD32\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\CrealityV4X_512K_GD32"

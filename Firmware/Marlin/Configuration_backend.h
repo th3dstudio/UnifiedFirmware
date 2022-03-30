2316ff11b10ea15314cb1b32fa8916fd74352854
houseofbugs <timothy@hoogland.email>
2022-03-30 03:06:43 -0500

    Bump Version, Minor

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.44"
+#define UNIFIED_VERSION "TH3D UFW 2.44a"
 
 /**
  * ABL Probe Settings

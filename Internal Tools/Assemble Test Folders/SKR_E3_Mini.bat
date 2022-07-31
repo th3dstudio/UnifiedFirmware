ed4698570342753d018e2345d24c56ab13c392c2
houseofbugs <timothy@hoogland.email>
2022-07-31 16:21:12 -0500

    Remove BTT Firmware due to them stealing IP from us.

--- Internal Tools/Assemble Test Folders/SKR_E3_Mini.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rmdir "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini" /s /q
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Board_Configs\SKR_E3_Mini\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini"

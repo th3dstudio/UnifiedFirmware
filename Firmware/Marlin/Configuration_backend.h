6c4bf8b9b9117f54d836a2b6e791976afe12a7d7
houseofbugs <me@timothyhoogland.com>
2022-09-09 21:34:02 -0500

    EZABL Fix for S1 Pro and S1 Plus

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -308,7 +308,7 @@
     #define Z_MIN_PROBE_ENDSTOP_INVERTING false
     #undef Z_MIN_ENDSTOP_INVERTING
     #define Z_MIN_ENDSTOP_INVERTING false
-  #elif (ENABLED(CR10S_PRO_STOCK_ABL) && ENABLED(CR10S_PRO)) || ENABLED(ENDER3_S1)
+  #elif (ENABLED(CR10S_PRO_STOCK_ABL) && ENABLED(CR10S_PRO)) || ANY(ENDER3_S1, ENDER3_S1_PRO, ENDER3_S1_PLUS)
     //Ender 3 S1 J713 header for Z Endstop is reverse logic via hardware for some reason. Need to invert the EZABL logic for it here.
     #undef Z_MIN_PROBE_ENDSTOP_INVERTING
     #define Z_MIN_PROBE_ENDSTOP_INVERTING false

a0821ad8d54eb17ca640b6d05a13ee3ca8b113aa
houseofbugs <me@timothyhoogland.com>
2022-09-27 18:17:03 -0500

    Fix Ender 7 Motor Directions

--- Board_Configs/CrealityV24S1_E7/Firmware/Marlin/Configuration.h
+++ Board_Configs/CrealityV24S1_E7/Firmware/Marlin/Configuration.h
@@ -368,12 +368,12 @@
 
   #define INVERT_X_DIR false
   #define INVERT_Y_DIR false
-  #define INVERT_Z_DIR true
+  #define INVERT_Z_DIR false
 
   #if ENABLED(REVERSE_E_MOTOR_DIRECTION)
-    #define INVERT_E0_DIR true
-  #else
     #define INVERT_E0_DIR false
+  #else
+    #define INVERT_E0_DIR true
   #endif
   
   #define INVERT_E1_DIR false

02ab2a1d7762e14ad1ea5a1e427ed4c663a38104
houseofbugs <me@timothyhoogland.com>
2022-09-27 17:53:26 -0500

    Bump version #

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.50a"
+#define UNIFIED_VERSION "TH3D UFW 2.51"
 
 /**
  * ABL Probe Settings

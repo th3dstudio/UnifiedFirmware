118b4b556579e17b55f5f01e5400075cc7dfcd58
houseofbugs <me@timothyhoogland.com>
2022-10-04 15:16:43 -0500

    E3 Mini V3 Ender 2 Pro Testing

--- Internal Tools/Compile Scripts/SKR_E3_Mini_V3.bat
+++ Internal Tools/Compile Scripts/SKR_E3_Mini_V3.bat
@@ -2,7 +2,7 @@
 
 rmdir "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3" /s /q
 xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Board_Configs\SKR_E3_Mini_V3\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3"
+xcopy /E /I /F "D:\Work\GitHub\UnifiedPaidConfigs\Board_Configs\SKR_E3_Mini_V3\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3"
 xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Internal Tools\Compile Scripts\SKR_E3_Mini_V3" "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3"
 D:
 cd "D:\Work\Printer Firmware\UF2TestBuilds\SKR_E3_Mini_V3\"

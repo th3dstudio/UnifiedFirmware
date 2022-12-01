b92d403ce615bcb5263e46691eed31bba23f6f02
houseofbugs <me@timothyhoogland.com>
2022-12-01 17:08:46 -0600

    Sync with 2.0.x-testing

--- Board_Configs/Creality_Melzi/Firmware/Marlin/Configuration.h
+++ Board_Configs/Creality_Melzi/Firmware/Marlin/Configuration.h
@@ -43,6 +43,7 @@
 //#define TM3DAERO
 //#define TM3DAERO_EXTENDED
 //#define PETSFANG //This is the RIGHT mounted version - if using the left mount please use the CUSTOM_PROBE option.
+//#define SPRITE_EXTRUDER_18MM_MOUNT // Mounts to the stock CRTouch bracket
 //#define CUSTOM_PROBE
 
 // Ender 5 - Leadscrew Setting

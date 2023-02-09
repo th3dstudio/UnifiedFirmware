7abe777ca0546c700d62afe3691a9cd4182f674c
Timothy Hoogland <me@timothyhoogland.com>
2023-02-09 16:48:22 -0600

    SV01 Pro Staging

---
+++ Internal Tools/Assemble Test Folders/SovolV4X.bat
@@ -0,0 +1,5 @@
+@echo off
+
+rmdir "D:\Work\Printer Firmware\UF2TestBuilds\SovolV4X" /s /q
+xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SovolV4X"
+xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Board_Configs\SovolV4X\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\SovolV4X"

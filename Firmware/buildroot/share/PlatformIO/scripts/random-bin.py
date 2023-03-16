590b2347eb658368cce7ddd6239a242752db9df3
Timothy Hoogland <me@timothyhoogland.com>
2023-03-15 19:45:15 -0500

    Made name more random
    Made name more random

--- Firmware/buildroot/share/PlatformIO/scripts/random-bin.py
+++ Firmware/buildroot/share/PlatformIO/scripts/random-bin.py
@@ -6,4 +6,4 @@ import pioutil
 if pioutil.is_pio_build():
     from datetime import datetime
     Import("env")
-    env['PROGNAME'] = datetime.now().strftime("UF2-%Y%m%d-%H%M%S")
+    env['PROGNAME'] = datetime.now().strftime("UF2-%M%S")

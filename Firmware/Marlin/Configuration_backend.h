e0dae4e968a1a1ebd0a4b1013576ec8f70acea0c
Timothy Hoogland <me@timothyhoogland.com>
2023-05-19 18:34:42 -0500

    Patch from https://github.com/MarlinFirmware/Marlin/pull/25122/commits
    https://github.com/MarlinFirmware/Marlin/pull/25122/commits

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.69"
+#define UNIFIED_VERSION "TH3D UFW 2.69a"
 
 /**
  * ABL Probe Settings

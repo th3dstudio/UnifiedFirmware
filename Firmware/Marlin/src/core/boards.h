605a4c216581fb2e757deeef2f084b7ef2b28719
Timothy Hoogland <me@timothyhoogland.com>
2023-06-17 03:17:44 -0500

    E3Freeruns Tested and working

--- Firmware/Marlin/src/core/boards.h
+++ Firmware/Marlin/src/core/boards.h
@@ -430,6 +430,7 @@
 #define BOARD_FYSETC_SPIDER_KING407   4242  // FYSETC Spider King407 (STM32F407ZG)
 #define BOARD_MKS_SKIPR_V1            4243  // MKS SKIPR v1.0 all-in-one board (STM32F407VE)
 #define BOARD_TRONXY_V10              4244  // TRONXY V10 (STM32F446ZE)
+#define BOARD_CREALITY_E3FREERUNS     4245  // Creality E3FreeRuns (STM32F401RE)
 
 //
 // ARM Cortex M7
--- Firmware/Marlin/src/pins/pins.h
+++ Firmware/Marlin/src/pins/pins.h
@@ -719,7 +719,8 @@
   #include "stm32f4/pins_MKS_SKIPR_V1_0.h"      // STM32F4                                env:mks_skipr_v1 env:mks_skipr_v1_nobootloader
 #elif MB(TRONXY_V10)
   #include "stm32f4/pins_TRONXY_V10.h"          // STM32F4                                env:STM32F446_tronxy
-
+#elif MB(CREALITY_E3FREERUNS)
+  #include "stm32f4/pins_CREALITY_E3FREERUNS.h" // STM32F4                                env:STM32F401RE_creality env:STM32F401RC_creality env:STM32F401RC_creality_jlink env:STM32F401RC_creality_stlink
 //
 // ARM Cortex M7
 //

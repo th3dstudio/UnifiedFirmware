b78fa60b1f3a64cf28e240e34e5516a18fb7751c
Timothy Hoogland <me@timothyhoogland.com>
2023-07-08 15:50:58 -0500

    ezboard script update

--- Internal Tools/Assemble Test Folders/TH3D_EZBoardLite.bat
+++ Internal Tools/Assemble Test Folders/TH3D_EZBoardLite.bat
@@ -2,4 +2,4 @@
 
 rmdir "D:\Work\Printer Firmware\UF2TestBuilds\TH3D_EZBoardLite" /s /q
 xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\TH3D_EZBoardLite"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedPaidConfigs\Board_Configs\TH3D_EZBoardLite\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\TH3D_EZBoardLite"
+xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Board_Configs\TH3D_EZBoardLite\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\TH3D_EZBoardLite"

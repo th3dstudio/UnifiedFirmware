f00be53b6014d4e7d1d972f7ab69a8a205365986
Timothy Hoogland <me@timothyhoogland.com>
2023-10-18 20:40:50 -0500

    Add Shaping Menu to 2560

--- Firmware/Marlin/Configuration_adv.h
+++ Firmware/Marlin/Configuration_adv.h
@@ -1239,9 +1239,7 @@
   #endif
   //#define SHAPING_MIN_FREQ  20        // By default the minimum of the shaping frequencies. Override to affect SRAM usage.
   //#define SHAPING_MAX_STEPRATE 10000  // By default the maximum total step rate of the shaped axes. Override to affect SRAM usage.
-  #if DISABLED(SPACE_SAVER_2560)
-    #define SHAPING_MENU                // Add a menu to the LCD to set shaping parameters.
-  #endif
+  #define SHAPING_MENU                // Add a menu to the LCD to set shaping parameters.
 #endif
 
 #define AXIS_RELATIVE_MODES { false, false, false, false }

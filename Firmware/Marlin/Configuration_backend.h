6746b2d12bd17485309e49545614160b70846fd7
Timothy Hoogland <me@timothyhoogland.com>
2023-10-23 20:09:51 -0500

    Mega 2560 Updates

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -6,7 +6,7 @@
 //======================= DO NOT MODIFY THIS FILE ===========================
 //===========================================================================
 
-#define UNIFIED_VERSION "TH3D UFW 2.77"
+#define UNIFIED_VERSION "TH3D UFW 2.77a"
 
 /**
  * ABL Probe Settings

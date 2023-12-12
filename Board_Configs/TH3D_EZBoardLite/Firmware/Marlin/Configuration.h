ceed144c7ce67db063b5afbc7d5eb4c6ff64667b
Timothy Hoogland <me@timothyhoogland.com>
2023-12-12 16:07:17 -0600

    Fix configs for EZBoard Upload on EZFirmware Site

--- Board_Configs/TH3D_EZBoardLite/Firmware/Marlin/Configuration.h
+++ Board_Configs/TH3D_EZBoardLite/Firmware/Marlin/Configuration.h
@@ -164,9 +164,7 @@
 /**
  * BLTouch/CRTouch
  * 
- * If you want to use the BLTouch/CRTouch uncomment the BLTOUCH line below.
- * You also need then enter in your sensor offsets below in the CUSTOM PROBE section.
- * 
+ * If you want to use the BLTouch/CRTouch uncomment the BLTOUCH line below. You also need then enter in your sensor offsets below in the CUSTOM PROBE section.
  * If you need to change your SERVO pin you can change that with the SERVO0_PIN Line below. Most people do NOT need to change/set this.
  */
 //#define BLTOUCH
@@ -175,8 +173,7 @@
 /**
  * Custom Probe Offset - EZABL/BLTouch/CRTouch
  * 
- * If you have a probe mount that is not pre-setup in the firmware then uncomment
- * the CUSTOM_PROBE option (or BLTOUCH if using one) above and enter your probe offsets below
+ * If you have a probe mount that is not pre-setup in the firmware then uncomment the CUSTOM_PROBE option (or BLTOUCH if using one) above and enter your probe offsets below
  */
 #if ANY(CUSTOM_PROBE, BLTOUCH)
   /**
@@ -323,11 +320,7 @@
 /**
  * EEPROM on SD Card
  * 
- * NOTE: THIS TAKES UP EXTRA SPACE ON THE CPU SO IF YOUR COMPILE FAILS DISABLE SOME OTHER OPTIONS IN THE FIRMWARE.
- * If your board has issues saving the EEPROM that could be due to a defective EEPROM chip. This will make an EEPROM.DAT file
- * You can use a SD card in the printer slot to save the EEPROM to that instead. Just uncomment the below line
- * and then leave an SD card in the printer at all times. If you have to take it out, power off the printer before removing
- * then put your GCode files on the SD card, insert the card again, and then power the printer up.
+ * If your board has issues saving the EEPROM that could be due to a defective EEPROM chip. This will make an EEPROM.DAT file. You can use a SD card in the printer slot to save the EEPROM to that instead. Just uncomment the below line and then leave an SD card in the printer at all times. If you have to take it out, power off the printer before removing then put your GCode files on the SD card, insert the card again, and then power the printer up.
  */
 //#define SDCARD_EEPROM_EMULATION
 
@@ -336,8 +329,7 @@
  * 
  * See here on how to use Input Shaping: https://www.th3dstudio.com/marlin-input-shaping-calculator/
  * 
- * Below are the frequency and damping settings for each axis.
- * Damping must have f at the end of the number and the range is 0.00-1.00.
+ * Below are the frequency and damping settings for each axis. Damping must have f at the end of the number and the range is 0.00-1.00.
  */
 //#define INPUT_SHAPING
 
@@ -364,8 +356,7 @@
  * If you need to adjust your XY home offsets from defaults then you can uncomment the HOME_ADJUST line below and enter your
  * custom XY offsets. This is provided for convenience and is unsupported with included product support.
  * 
- * How to use - measure (home XY then jog using the LCD 1mm at a time) the X and Y distance the nozzle is off
- * the build plate and then put those as NEGATIVE values below on the X_HOME_LOCATION and Y_HOME_LOCATION values
+ * How to use - measure (home XY then jog using the LCD 1mm at a time) the X and Y distance the nozzle is off the build plate and then put those as NEGATIVE values below on the X_HOME_LOCATION and Y_HOME_LOCATION values
  * Positive values will NOT work (move your endstops to fix a positive offset or change your bed size above).
  */
 //#define HOME_ADJUST
@@ -400,12 +391,9 @@
 /**
  * Manual Mesh Leveling 
  *
- * If you want to use manual mesh leveling you can enable the below option. 
- * This is for generating a MANUAL mesh WITHOUT a probe. To change the mesh inset value change the EZABL_PROBE_EDGE setting above.
+ * If you want to use manual mesh leveling you can enable the below option. This is for generating a MANUAL mesh WITHOUT a probe. To change the mesh inset value change the EZABL_PROBE_EDGE setting above.
  * 
- * Mesh Bed Leveling Documentation: http://marlinfw.org/docs/gcode/G029-mbl.html 
- * 
- * NOTE: If you want to automate the leveling process our EZABL kits do this for you. Check them out here: http://EZABL.TH3DStudio.com
+ * Mesh Bed Leveling Documentation: http://marlinfw.org/docs/gcode/G029-mbl.html NOTE: If you want to automate the leveling process our EZABL kits do this for you. Check them out here: http://EZABL.TH3DStudio.com
  */
 //#define MANUAL_MESH_LEVELING
 
@@ -445,9 +433,6 @@
   #define XY_DIAG_BD 282.8427124746
   #define XY_SIDE_AD 200
 
-  // Or, set the XY skew factor directly:
-  //#define XY_SKEW_FACTOR 0.0
-
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
     #define XZ_DIAG_AC 282.8427124746
@@ -455,10 +440,6 @@
     #define YZ_DIAG_AC 282.8427124746
     #define YZ_DIAG_BD 282.8427124746
     #define YZ_SIDE_AD 200
-
-    // Or, set the Z skew factors directly:
-    //#define XZ_SKEW_FACTOR 0.0
-    //#define YZ_SKEW_FACTOR 0.0
   #endif
 
   // Enable this option for M852 to set skew at runtime
@@ -506,7 +487,7 @@
 #if ENABLED(BLTOUCH) && DISABLED(CUSTOM_PROBE)
   #define CUSTOM_PROBE
 #endif
- 
+
 //Set Ender 3 Pro Machine Setting
 #if ENABLED(ENDER3_PRO)
   #define ENDER3
@@ -629,7 +610,7 @@
     #define Z_MAX_POS 220
     #define PRINTER_VOLTAGE_12
   #endif
-  
+
   #if ENABLED(ENDER2_PRO)
     #define X_BED_SIZE 165
     #define Y_BED_SIZE 168
--- Board_Configs/TH3D_EZBoardV2/Firmware/Marlin/Configuration.h
+++ Board_Configs/TH3D_EZBoardV2/Firmware/Marlin/Configuration.h
@@ -193,9 +193,7 @@
 /**
  * BLTouch/CRTouch
  * 
- * If you want to use the BLTouch/CRTouch uncomment the BLTOUCH line below.
- * You also need then enter in your sensor offsets below in the CUSTOM PROBE section.
- * 
+ * If you want to use the BLTouch/CRTouch uncomment the BLTOUCH line below. You also need then enter in your sensor offsets below in the CUSTOM PROBE section.
  * If you need to change your SERVO pin you can change that with the SERVO0_PIN Line below. Most people do NOT need to change/set this.
  */
 //#define BLTOUCH
@@ -204,8 +202,7 @@
 /**
  * Custom Probe Offset - EZABL/BLTouch/CRTouch
  * 
- * If you have a probe mount that is not pre-setup in the firmware then uncomment
- * the CUSTOM_PROBE option (or BLTOUCH if using one) above and enter your probe offsets below
+ * If you have a probe mount that is not pre-setup in the firmware then uncomment the CUSTOM_PROBE option (or BLTOUCH if using one) above and enter your probe offsets below
  */
 #if ANY(CUSTOM_PROBE, BLTOUCH)
   /**
@@ -362,11 +359,7 @@
 /**
  * EEPROM on SD Card
  * 
- * NOTE: THIS TAKES UP EXTRA SPACE ON THE CPU SO IF YOUR COMPILE FAILS DISABLE SOME OTHER OPTIONS IN THE FIRMWARE.
- * If your board has issues saving the EEPROM that could be due to a defective EEPROM chip. This will make an EEPROM.DAT file
- * You can use a SD card in the printer slot to save the EEPROM to that instead. Just uncomment the below line
- * and then leave an SD card in the printer at all times. If you have to take it out, power off the printer before removing
- * then put your GCode files on the SD card, insert the card again, and then power the printer up.
+ * If your board has issues saving the EEPROM that could be due to a defective EEPROM chip. This will make an EEPROM.DAT file. You can use a SD card in the printer slot to save the EEPROM to that instead. Just uncomment the below line and then leave an SD card in the printer at all times. If you have to take it out, power off the printer before removing then put your GCode files on the SD card, insert the card again, and then power the printer up.
  */
 //#define SDCARD_EEPROM_EMULATION
 
@@ -375,8 +368,7 @@
  * 
  * See here on how to use Input Shaping: https://www.th3dstudio.com/marlin-input-shaping-calculator/
  * 
- * Below are the frequency and damping settings for each axis.
- * Damping must have f at the end of the number and the range is 0.00-1.00.
+ * Below are the frequency and damping settings for each axis. Damping must have f at the end of the number and the range is 0.00-1.00.
  */
 //#define INPUT_SHAPING
 
@@ -403,8 +395,7 @@
  * If you need to adjust your XY home offsets from defaults then you can uncomment the HOME_ADJUST line below and enter your
  * custom XY offsets. This is provided for convenience and is unsupported with included product support.
  * 
- * How to use - measure (home XY then jog using the LCD 1mm at a time) the X and Y distance the nozzle is off
- * the build plate and then put those as NEGATIVE values below on the X_HOME_LOCATION and Y_HOME_LOCATION values
+ * How to use - measure (home XY then jog using the LCD 1mm at a time) the X and Y distance the nozzle is off the build plate and then put those as NEGATIVE values below on the X_HOME_LOCATION and Y_HOME_LOCATION values
  * Positive values will NOT work (move your endstops to fix a positive offset or change your bed size above).
  */
 //#define HOME_ADJUST
@@ -439,12 +430,9 @@
 /**
  * Manual Mesh Leveling 
  *
- * If you want to use manual mesh leveling you can enable the below option. 
- * This is for generating a MANUAL mesh WITHOUT a probe. To change the mesh inset value change the EZABL_PROBE_EDGE setting above.
+ * If you want to use manual mesh leveling you can enable the below option. This is for generating a MANUAL mesh WITHOUT a probe. To change the mesh inset value change the EZABL_PROBE_EDGE setting above.
  * 
- * Mesh Bed Leveling Documentation: http://marlinfw.org/docs/gcode/G029-mbl.html 
- * 
- * NOTE: If you want to automate the leveling process our EZABL kits do this for you. Check them out here: http://EZABL.TH3DStudio.com
+ * Mesh Bed Leveling Documentation: http://marlinfw.org/docs/gcode/G029-mbl.html NOTE: If you want to automate the leveling process our EZABL kits do this for you. Check them out here: http://EZABL.TH3DStudio.com
  */
 //#define MANUAL_MESH_LEVELING
 
@@ -484,9 +472,6 @@
   #define XY_DIAG_BD 282.8427124746
   #define XY_SIDE_AD 200
 
-  // Or, set the XY skew factor directly:
-  //#define XY_SKEW_FACTOR 0.0
-
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
     #define XZ_DIAG_AC 282.8427124746
@@ -494,10 +479,6 @@
     #define YZ_DIAG_AC 282.8427124746
     #define YZ_DIAG_BD 282.8427124746
     #define YZ_SIDE_AD 200
-
-    // Or, set the Z skew factors directly:
-    //#define XZ_SKEW_FACTOR 0.0
-    //#define YZ_SKEW_FACTOR 0.0
   #endif
 
   // Enable this option for M852 to set skew at runtime
@@ -1080,24 +1061,8 @@
       #define FIL_RUNOUT_STATE HIGH // Pin state indicating that filament is NOT present.
     #endif
     
-    #define FIL_RUNOUT_PULLUP               // Use internal pullup for filament runout pins.
-    //#define FIL_RUNOUT_PULLDOWN           // Use internal pulldown for filament runout pins.
-
-    // Set one or more commands to execute on filament runout.
-    // (After 'M412 H' Marlin will ask the host to handle the process.)
+    #define FIL_RUNOUT_PULLUP
     #define FILAMENT_RUNOUT_SCRIPT "M600"
-
-    // After a runout is detected, continue printing this length of filament
-    // before executing the runout script. Useful for a sensor at the end of
-    // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-    //#define FILAMENT_RUNOUT_DISTANCE_MM 25
-
-    #ifdef FILAMENT_RUNOUT_DISTANCE_MM
-      // Enable this option to use an encoder disc that toggles the runout pin
-      // as the filament moves. (Be sure to set FILAMENT_RUNOUT_DISTANCE_MM
-      // large enough to avoid false positives.)
-      //#define FILAMENT_MOTION_SENSOR
-    #endif
   #endif
 
   #if ENABLED(EZNEO_220)
@@ -1114,17 +1079,6 @@
       #define NEOPIXEL_STARTUP_TEST  // Cycle through colors at startup
     #endif
 
-    /**
-     * Printer Event LEDs
-     *
-     * During printing, the LEDs will reflect the printer status:
-     *
-     *  - Gradually change from blue to violet as the heated bed gets to target temp
-     *  - Gradually change from violet to red as the hotend gets to temperature
-     *  - Change to white to illuminate work surface
-     *  - Change to green once print has finished
-     *  - Turn off after the print has finished and the user has pushed a button
-     */
     #if ANY(BLINKM, RGB_LED, RGBW_LED, PCA9632, PCA9533, NEOPIXEL_LED)
       #define PRINTER_EVENT_LEDS
     #endif
@@ -1396,24 +1350,8 @@
       #define FIL_RUNOUT_STATE HIGH // Pin state indicating that filament is NOT present.
     #endif
     
-    #define FIL_RUNOUT_PULLUP               // Use internal pullup for filament runout pins.
-    //#define FIL_RUNOUT_PULLDOWN           // Use internal pulldown for filament runout pins.
-
-    // Set one or more commands to execute on filament runout.
-    // (After 'M412 H' Marlin will ask the host to handle the process.)
+    #define FIL_RUNOUT_PULLUP
     #define FILAMENT_RUNOUT_SCRIPT "M600"
-
-    // After a runout is detected, continue printing this length of filament
-    // before executing the runout script. Useful for a sensor at the end of
-    // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-    //#define FILAMENT_RUNOUT_DISTANCE_MM 25
-
-    #ifdef FILAMENT_RUNOUT_DISTANCE_MM
-      // Enable this option to use an encoder disc that toggles the runout pin
-      // as the filament moves. (Be sure to set FILAMENT_RUNOUT_DISTANCE_MM
-      // large enough to avoid false positives.)
-      //#define FILAMENT_MOTION_SENSOR
-    #endif
   #endif
 
   #if ENABLED(EZNEO_220)
@@ -1429,18 +1367,6 @@
       #define NEOPIXEL_BRIGHTNESS 255  // Initial brightness (0-255)
       #define NEOPIXEL_STARTUP_TEST  // Cycle through colors at startup
     #endif
-
-    /**
-     * Printer Event LEDs
-     *
-     * During printing, the LEDs will reflect the printer status:
-     *
-     *  - Gradually change from blue to violet as the heated bed gets to target temp
-     *  - Gradually change from violet to red as the hotend gets to temperature
-     *  - Change to white to illuminate work surface
-     *  - Change to green once print has finished
-     *  - Turn off after the print has finished and the user has pushed a button
-     */
     #if ANY(BLINKM, RGB_LED, RGBW_LED, PCA9632, PCA9533, NEOPIXEL_LED)
       #define PRINTER_EVENT_LEDS
     #endif

0ea179f3bf869840ee7f73c11bd727851bd68933
Timothy Hoogland <me@timothyhoogland.com>
2024-03-11 14:05:20 -0500

    EOL Alfawise and ADIM

--- Internal Tools/Assemble Test Folders/ADIM_Ramps.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rmdir "D:\Work\Printer Firmware\UF2TestBuilds\ADIM_Ramps" /s /q
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\ADIM_Ramps"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedPaidConfigs\Board_Configs\ADIM_Ramps\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\ADIM_Ramps"

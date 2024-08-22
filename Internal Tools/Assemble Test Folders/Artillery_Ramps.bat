a0a64c5a545bfbee44348cafa88519e8d262488f
Timothy Hoogland <me@timothyhoogland.com>
2024-08-21 20:40:06 -0500

    Remove discontinued setups

--- Internal Tools/Assemble Test Folders/Artillery_Ramps.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rmdir "D:\Work\Printer Firmware\UF2TestBuilds\Artillery_Ramps" /s /q
-xcopy /E /I /F "D:\Work\GitHub\UnifiedFirmware\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\Artillery_Ramps"
-xcopy /E /I /F "D:\Work\GitHub\UnifiedPaidConfigs\Board_Configs\Artillery_Ramps\Firmware" "D:\Work\Printer Firmware\UF2TestBuilds\Artillery_Ramps"

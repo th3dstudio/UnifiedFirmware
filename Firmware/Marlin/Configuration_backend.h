c0907ebfcdfa98746eb6cb2ea1d38ead11f4e7c6
Timothy Hoogland <me@timothyhoogland.com>
2025-02-05 13:38:57 -0600

    2.95 release
    Upped default MAX hotend temp to 290C
    Updated V4X Board notes for GD32 CPUs

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -7,7 +7,7 @@
 //===========================================================================
 
 #ifndef UNIFIED_VERSION
-  #define UNIFIED_VERSION "TH3D UFW 2.94a"
+  #define UNIFIED_VERSION "TH3D UFW 2.95"
 #endif
 
 /**

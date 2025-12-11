bd0729e370e4dfdbe5dc01d6a7be2cdc1efd717b
Timothy Hoogland <me@timothyhoogland.com>
2025-12-11 11:31:20 -0600

    2.98 release

--- Firmware/Marlin/Configuration_backend.h
+++ Firmware/Marlin/Configuration_backend.h
@@ -7,7 +7,7 @@
 //===========================================================================
 
 #ifndef UNIFIED_VERSION
-  #define UNIFIED_VERSION "TH3D UFW 2.98t"
+  #define UNIFIED_VERSION "TH3D UFW 2.98"
 #endif
 
 /**
